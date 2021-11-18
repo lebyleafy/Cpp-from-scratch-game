7cf715fa4d661234e13b8d1692c08ce35b068010
lea <73567807+lebyleafy@users.noreply.github.com>
2021-11-18 21:36:43 +0700

    Add files via upload

---
+++ lau dai tinh ai/lau dai tinh ai/SwapChain.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <d3d11.h>
+
+class SwapChain
+{
+public:
+	SwapChain();
+	//Initialize SwapChain for a window
+	bool init(HWND hwnd, UINT width, UINT height);
+	//Release the swap chain
+	bool release();
+	~SwapChain();
+private:
+	IDXGISwapChain* m_swap_chain;
+};
